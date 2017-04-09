8bf24cceda3701f882b7537c4f86d7dba101ccd6
QH <quans.box@gmail.com>
2017-04-09 19:47:38 -0400

    remove debug code

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,9 @@ void check_arguments(int argc, char* argv[]) {
   if (argc == 1) {
     std::cerr << usage_instructions << endl;
 
-	argv[1] = "d:\\sample-laser-radar-measurement-data-1.txt";
-	argv[2] = "d:\\output1.txt";
-	has_valid_args = true;
+	// argv[1] = "d:\\sample-laser-radar-measurement-data-1.txt";
+	// argv[2] = "d:\\output1.txt";
+    has_valid_args = false;
 
   } else if (argc == 2) {
     std::cerr << "Please include an output file.\n" << usage_instructions << endl;
